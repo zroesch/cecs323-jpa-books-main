a1ca628ed31076debb2e752b4d69961948080b92
iamkellpham <Bich-Tram.Pham01@student.csulb.edu>
2022-06-15 00:06:48 -0700

    Finish Case 2: Show Info

--- src/main/java/csulb/cecs323/app/BookJPA.java
+++ src/main/java/csulb/cecs323/app/BookJPA.java
@@ -90,6 +90,9 @@ public class BookJPA {
         List<Publishers> publishers = new ArrayList<>();
         publishers.add(new Publishers("Penguin House Publisher", "714-666-777", "penguinepublisher@gmail.com"));
         books.createEntity((publishers));
+
+        List<Books> bookPhys = new ArrayList<>();
+        books.createEntity(bookPhys);
         tx.commit();
 
         //Prompt Menu
@@ -103,64 +106,149 @@ public class BookJPA {
         int choice = Integer.parseInt(input.next());
         int subChoice = 0;
         switch (choice) {
-            //Adding new Objects
-            case 1: {
+            // Add a new object to the db
+            case 1:
                 tx.begin();
-                String name;
-                String email;
+                System.out.println("Select a new object type to create:\n1. Authoring Entity\n2. Publisher\n3. Book");
+                int typeChoice = Integer.parseInt(getUserString());
 
-                System.out.println("1. Add a Writing Group");
-                System.out.println("2. Add an Individual Author");
-                System.out.println("3. Add an Ad Hoc Team");
-                subChoice = Integer.parseInt(input.next());
-
-                switch (subChoice) {
-                    case 1:
-                        System.out.println("Enter Writing Group name:");
-                        name = getUserString();
-                        System.out.println("Enter Writing Group email:");
-                        email = getUserString();
+                // Creating a new authoring entity
+                if (typeChoice == 1) {
+                    // Variables to choose an authoring entity/type of entity
+                    String email;
+                    String name;
+                    String type = "null";
+                    int typeInt = 0;
+                    // selecting the type
+                    while (typeInt < 1 || typeInt > 3) {
+                        System.out.println("Select authoring entity type: \n1. Individual Author\n2. Ad Hoc Team\n3. Writing Group");
+                        typeInt = Integer.parseInt(getUserString());
+                        switch (typeInt) {
+                            case 1:
+                                type = "Individual Author";
+                                break;
+                            case 2:
+                                type = "Ad Hoc Team";
+                                break;
+                            case 3:
+                                type = "Writing Group";
+                                break;
+                            default:
+                                System.out.println("invalid input");
+                                break;
+                        }
+                    }
+                    System.out.println("Enter authoring entity name:");
+                    name = getUserString();
+                    System.out.println("Enter authoring entity email:");
+                    email = getUserString();
+                    if (type.equals("Individual Author")) {
+                        authorings.add(new IndividualAuthors(name, email));
+                    } else if (type.equals("Ad Hoc Team")) {
+                        authorings.add(new AdHocTeams(name, email));
+                    } else if (type.equals("Writing Group")) {
                         System.out.println("Enter the name of the head writer.");
                         String headWriter = getUserString();
                         System.out.println ("Enter the year the writing group was formed:");
                         int yearFormed = Integer.parseInt(getUserString());
                         authorings.add(new WritingGroups(name, email, headWriter, yearFormed));
-                        break;
-                    case 2:
-                        System.out.println("Enter individual author name:");
-                        name = getUserString();
-                        System.out.println("Enter individual author email:");
-                        email = getUserString();
-                        authorings.add(new IndividualAuthors(name, email));
-                        break;
-                    case 3:
-                        System.out.println("Enter Ad Hoc Team name:");
-                        name = getUserString();
-                        System.out.println("Enter Ad Hoc Team email:");
-                        email = getUserString();
-                        authorings.add(new AdHocTeams(name, email));
-                        break;
+                    }
+                    books.createEntity (authorings); // persisting new authoring entity
+                }
+                // New publisher
+                else if (typeChoice == 2) {
+                    // Variables for new publisher.
+                    String name;
+                    String email;
+                    String phone;
+                    // Gathering variables.
+                    System.out.println("Enter the name of the publisher:");
+                    name = getUserString();
+                    System.out.println("Enter the email of the publisher:");
+                    email = getUserString();
+                    System.out.println("Enter the phone number of the publisher:");
+                    phone = getUserString();
+
+                    publishers.add(new Publishers(name, phone, email));
+                    books.createEntity(publishers);
+                }
+                // New book.
+                else if (typeChoice == 3) {
+                    // Variables to use in initializing our new book.
+                    Publishers bookPublisher = new Publishers();
+                    AuthoringEntities author = new AuthoringEntities();
+                    String title;
+                    String publisherName;
+                    String authorEmail;
+                    int isbn;
+                    int yearPublished;
+                    // Gathering variables.
+                    System.out.println("Enter the book title:");
+                    title = getUserString();
+                    System.out.println("Enter the book isbn:");
+                    isbn = Integer.parseInt(getUserString());
+                    System.out.println("Enter the year published:");
+                    yearPublished = Integer.parseInt(getUserString());
+                    System.out.println("LIST OF PUBLISHERS:");
+                    for (Publishers p : publishers) {
+                        System.out.println(p);
+                    }
+                    System.out.println("Select the publisher's name:");
+                    publisherName = getUserString();
+                    for (Publishers p : publishers) {
+                        if (p.getName().equals(publisherName)) {
+                            bookPublisher = p;
+                        }
+                    }
+                    System.out.println("LIST OF AUTHORING ENTITIES:");
+                    for (AuthoringEntities a : authorings) {
+                        System.out.println(a.toString());
+                    }
+                    System.out.println("Select the author's email");
+                    authorEmail = getUserString();
+                    for (AuthoringEntities a : authorings) {
+                        if (a.getEmail().equals(authorEmail)) {
+                            author = a;
+                        }
+                    }
+                    bookPhys.add(new Books(bookPublisher, author, isbn, title, yearPublished));
+                    books.createEntity(bookPhys);
+                } else {
+                    System.out.println("Invalid input.");
                 }
                 tx.commit();
                 break;
-            }
 
+            //Display Object's info
             case 2: {
-                System.out.println("1. Add a Writing Group");
-                System.out.println("2. Add an Individual Author");
-                System.out.println("3. Add an Ad Hoc Team");
-                subChoice = Integer.parseInt(input.next());
-                switch (subChoice) {
-                    case 1:
+                System.out.println("1. Display all information for a specific Publisher.\n" +
+                        "2. Display all information for a specific Book.\n" +
+                        "3. Display all information for a specific Writing Group.\n" +
+                        "4. Finish");
+                Scanner in2 = new Scanner(System.in);
+                int choice2 = Integer.parseInt(in2.next());
+                Scanner in3 = new Scanner(System.in);
+                switch(choice2) {
+                    case 1: //Display information for specific publisher
+                        System.out.println("Select a publisher by entering their name:");
+                        String name = in3.nextLine();
+                        for (Publishers p : publishers) {
+                            if (p.getName().equals(name)) {
+                                System.out.println(p);
+                            }
+                        }
                         break;
-                    case 2:
+                    case 2: //Display information for specific book
+                        System.out.println("Select a book by entering its isbn:");
+                        int isbn = in3.nextInt();
+                        for (Books b : bookPhys) {
+                            if (b.getIsbn() == isbn) {
+                                System.out.println(b);
+                            }
+                        }
                         break;
-                    case 3:
-                        break;
-
+                    }
                 }
-                break;
-            }
 
             case 3: {
                 System.out.println("Please enter the candidate keys in order to delete a book ");
